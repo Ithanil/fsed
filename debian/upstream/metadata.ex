7b94492905c5b9d19dfec46624cf10830b981a8e
Jan Kessler <jakessle@uni-mainz.de>
2024-08-05 11:23:03 +0200

    add initial Debian package files (adapted from dh_make boilerplate)

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,6 @@
+# Bug-Database: https://github.com/Ithanil/ssed/issues
+# Bug-Submit: https://github.com/Ithanil/ssed/issues/new
+# Changelog: https://github.com/Ithanil/ssed/blob/master/CHANGES
+# Documentation: https://github.com/Ithanil/ssed/README.md
+# Repository-Browse: https://github.com/Ithanil/ssed
+# Repository: https://github.com/Ithanil/ssed.git
